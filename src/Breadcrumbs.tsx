234fea580f8d0159666f1ece91ac79b48c3cb088
Anton Strogonoff <anton@strogonoff.name>
2021-01-31 07:26:16 +0000

    refactor: move breadcrumbs into own module

---
+++ src/Breadcrumbs.tsx
@@ -0,0 +1,64 @@
+import Electron from 'electron';
+import { remote } from 'electron';
+import * as R from 'ramda';
+import React from 'react';
+import { Button, ButtonGroup, ControlGroup, IButtonProps, IconName } from '@blueprintjs/core';
+
+
+const Breadcrumbs: React.FC<BreadcrumbsProps> = function ({ crumbs, onNavigate }) {
+  function handleCrumbNavigate(crumb: Crumb) {
+    const currentIndex = crumbs.indexOf(crumb);
+    if (currentIndex >= 0) {
+      const newCrumbs = R.take(currentIndex + 1, crumbs);
+      onNavigate(newCrumbs);
+    }
+  }
+  return (
+    <ControlGroup>
+      {crumbs.map(crumb => <Crumb
+        crumb={crumb}
+        onNavigate={() => handleCrumbNavigate(crumb)} />
+      )}
+    </ControlGroup>
+  );
+};
+
+export const Crumb: React.FC<{ crumb: Crumb; onNavigate: () => void; }> = function ({ crumb, onNavigate }) {
+  const crumbButtonProps: IButtonProps = {
+    disabled: !crumb.canNavigate,
+    text: crumb.label,
+    onClick: onNavigate,
+  };
+  function invokeSiblingMenu() {
+    if (crumb.siblingMenu) {
+      remote.Menu.buildFromTemplate(crumb.siblingMenu).popup();
+    }
+  }
+  if (crumb.siblingMenu) {
+    return (
+      <ButtonGroup>
+        <Button {...crumbButtonProps} />
+        <Button icon="chevron-down" onClick={invokeSiblingMenu} />
+      </ButtonGroup>
+    );
+  } else {
+    return <Button {...crumbButtonProps} />;
+  }
+};
+
+export interface Crumb {
+  path: string;
+  label: JSX.Element | string;
+  icon: IconName;
+  canNavigate?: boolean;
+  siblingMenu?: Electron.MenuItemConstructorOptions[];
+  view?: React.FC<any>;
+}
+
+export interface BreadcrumbsProps {
+  crumbs: Crumb[];
+  onNavigate: (newCrumbs: Crumb[]) => void;
+}
+
+
+export default Breadcrumbs;
--- src/MainView.tsx
+++ src/MainView.tsx
@@ -1,10 +1,9 @@
-import type Electron from 'electron';
-import { remote } from 'electron';
 import * as R from 'ramda';
 import React, { useState, useContext } from 'react';
-import { Button, ButtonGroup, ControlGroup, IButtonProps, IconName, NonIdealState } from '@blueprintjs/core';
+import { NonIdealState } from '@blueprintjs/core';
 import { DatasetContext } from '@riboseinc/paneron-extension-kit/context';
 import type { DatasetContext as DatasetContextSpec } from '@riboseinc/paneron-extension-kit/types/renderer';
+import Breadcrumbs, { Crumb } from './Breadcrumbs';
 
 
 const ErrorLoadingObjectView: React.FC<DatasetContextSpec & { objectPath: string }> = function ({ objectPath }) {
@@ -55,65 +54,6 @@ function () {
     </div>
   );
 };
-
-
-interface Crumb {
-  path: string
-  label: JSX.Element | string
-  icon: IconName
-  canNavigate?: boolean
-  siblingMenu?: Electron.MenuItemConstructorOptions[]
-  view?: React.FC<any>
-}
-
-
-interface BreadcrumbsProps {
-  crumbs: Crumb[]
-  onNavigate: (newCrumbs: Crumb[]) => void
-}
-const Breadcrumbs: React.FC<BreadcrumbsProps> = function ({ crumbs, onNavigate }) {
-  function handleCrumbNavigate(crumb: Crumb) {
-    const currentIndex = crumbs.indexOf(crumb);
-    if (currentIndex >= 0) {
-      const newCrumbs = R.take(currentIndex + 1, crumbs);
-      onNavigate(newCrumbs);
-    }
-  }
-  return (
-    <ControlGroup>
-      {crumbs.map(crumb =>
-        <Crumb
-          crumb={crumb}
-          onNavigate={() => handleCrumbNavigate(crumb)}
-        />
-      )}
-    </ControlGroup>
-  );
-};
-
-
-const Crumb: React.FC<{ crumb: Crumb, onNavigate: () => void }> = function ({ crumb, onNavigate }) {
-  const crumbButtonProps: IButtonProps = {
-    disabled: !crumb.canNavigate,
-    text: crumb.label,
-    onClick: onNavigate,
-  };
-  function invokeSiblingMenu() {
-    if (crumb.siblingMenu) {
-      remote.Menu.buildFromTemplate(crumb.siblingMenu).popup();
-    }
-  }
-  if (crumb.siblingMenu) {
-    return (
-      <ButtonGroup>
-        <Button {...crumbButtonProps} />
-        <Button icon="chevron-down" onClick={invokeSiblingMenu} />
-      </ButtonGroup>
-    );
-  } else {
-    return <Button {...crumbButtonProps} />;
-  }
-}
 
 
 interface HomeProps extends DatasetContextSpec {}
