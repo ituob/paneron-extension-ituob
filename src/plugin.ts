9b3c9aaf6acdead4d5cd7edc0242bdbb28e5e5ca
Anton Strogonoff <anton@strogonoff.name>
2021-10-27 19:48:42 +0800

    chore: update extension spec, switch to tabbed view

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,28 +1,8 @@
-import yaml from 'js-yaml';
 import { makeExtension } from '@riboseinc/paneron-extension-kit';
-import { makeNestedSerializableObjectSpec } from '@riboseinc/paneron-extension-kit/object-specs/nested';
-import { decoder, encoder } from '@riboseinc/paneron-extension-kit/util';
-
-
-const issueObjectSpec = makeNestedSerializableObjectSpec({
-  matches: {
-    pathPrefix: '/issues/',
-  },
-  views: async () => ({
-    default: {
-      default: (await import('./IssueEditor')).default,
-    },
-  }),
-  serializePart: (data) => encoder.encode(yaml.dump(data, { noRefs: true })),
-  deserializePart: (buffer) => yaml.load(decoder.decode(buffer)),
-});
 
 
 export default makeExtension({
   requiredHostAppVersion: '1.0.0-beta1',
-  mainView: () => import('./MainView'),
-  objects: [
-    issueObjectSpec,
-  ],
+  mainView: () => import('./IssueBasedTabbedView'),
   name: "ITU OB",
 });
