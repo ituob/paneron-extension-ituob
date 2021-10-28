f8270a775bbe37443b43b9463ad4935a5bf47bfc
Anton Strogonoff <anton@strogonoff.name>
2021-10-28 17:47:03 +0800

    feat(useIssueData): support requesting multiple

--- src/useIssueData.ts
+++ src/useIssueData.ts
@@ -1,9 +1,44 @@
-import log from 'electron-log';
 import { useContext } from 'react';
 import { DatasetContext } from '@riboseinc/paneron-extension-kit/context';
 import { ValueHook } from '@riboseinc/paneron-extension-kit/types';
 import { AnnexesBlock, ScheduledIssue, MessageBlock, OBIssue, IssueMeta } from './types/issues';
 
+
+export default function useIssueData
+(opts: { issueIDs: number[] }):
+ValueHook<{ issues: { [issueID: number]: OBIssue | null } }> {
+  const { useObjectData } = useContext(DatasetContext);
+
+  const objectPathsPerIssue: { [issueID: number]: Record<IssueObjectID, string> } =
+    opts.issueIDs.
+      map(iid => ({ [iid]: getObjectPaths(iid) })).
+      reduce((prev, curr) => ({ ...prev, ...curr }));
+
+  const allObjectPaths: string[] =
+    Object.entries(objectPathsPerIssue).
+      map(([, paths]) => Object.values(paths)).
+      reduce((prev, curr) => [ ...prev, ...curr ]);
+
+  const resp = useObjectData({ objectPaths: allObjectPaths });
+
+  const issueDatasets: { [issueID: number]: OBIssue | null } =
+    Object.entries(resp.value.data).
+      map(([ objPath, dataset ]) => ({
+        [getIssueIDFromObjectPath(objPath)]:
+          dataset && isIssueDataset(dataset)
+            ? dataToIssue(dataset)
+            : null,
+      })).
+      reduce((prev, curr) => ({ ...prev, ...curr }));
+
+  return {
+    ...resp,
+    value: { issues: issueDatasets },
+  };
+}
+
+
+// Types & constants
 
 interface IssueDataset {
   meta: ScheduledIssue & IssueMeta
@@ -21,20 +56,12 @@ const ISSUE_FILENAMES: Record<IssueObjectID, string> = {
   annexes: 'annexes.yaml',
 }
 
-function getIssueRootPath(issueID: number): string {
-  return `/issues/${issueID}/`;
-}
-
-function getObjectPaths(issueID: number): Record<IssueObjectID, string> {
-  const root = getIssueRootPath(issueID);
-  return Object.entries(ISSUE_FILENAMES).
-    map(([id, fn]) => ({ [id]: `${root}${fn}` })).
-    reduce((prev, curr) => ({ ...prev, ...curr }), {}) as Record<IssueObjectID, string>;
-}
-
 function isIssueDataset(data: any): data is IssueDataset {
   return data.meta && data.general && data.amendments && data.annexes ? true : false;
 }
+
+
+// Helpers
 
 function dataToIssue(data: IssueDataset): OBIssue {
   const issue: OBIssue = {
@@ -46,26 +73,22 @@ function dataToIssue(data: IssueDataset): OBIssue {
   return issue;
 }
 
+function getObjectPaths(issueID: number): Record<IssueObjectID, string> {
+  const root = getIssueRootPath(issueID);
+  return Object.entries(ISSUE_FILENAMES).
+    map(([id, fn]) => ({ [id]: `${root}${fn}` })).
+    reduce((prev, curr) => ({ ...prev, ...curr }), {}) as Record<IssueObjectID, string>;
+}
 
-export default function useIssueData
-(issueID: number):
-ValueHook<OBIssue | null> {
-  const { useObjectData } = useContext(DatasetContext);
-  const paths = getObjectPaths(issueID);
-  const resp = useObjectData({ objectPaths: Object.values(paths) });
-  const maybeIssueDataset = resp.value.data;
+function getIssueRootPath(issueID: number): string {
+  return `/issues/${issueID}/`;
+}
 
-  if (isIssueDataset(maybeIssueDataset)) {
-    const issueData = dataToIssue(maybeIssueDataset);
-    return {
-      ...resp,
-      value: issueData,
-    };
+function getIssueIDFromObjectPath(objPath: string): number {
+  const issueID = objPath.split('/')[2];
+  if (issueID) {
+    return parseInt(issueID, 10);
   } else {
-    log.error("useIssueData: Unable to retrieve issue data", issueID);
-    return {
-      ...resp,
-      value: null,
-    };
+    throw new Error("Unable to resolve issue ID");
   }
 }
